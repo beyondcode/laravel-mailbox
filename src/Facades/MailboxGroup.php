30467ee0618987e26d82693175e434c556bc4090
Marko Dupor <marko.dupor@gmail.com>
2020-12-11 09:38:12 +0100

    Updated facades to have method completion

--- src/Facades/MailboxGroup.php
+++ src/Facades/MailboxGroup.php
@@ -2,9 +2,16 @@
 
 namespace BeyondCode\Mailbox\Facades;
 
+use BeyondCode\Mailbox\InboundEmail;
+use BeyondCode\Mailbox\Routing\Mailbox;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static void run(InboundEmail $email)
+ * @method static self add(Mailbox $mailbox)
+ * @method static self fallback($action)
+ * @method static self continuousMatching()
+ *
  * @see \BeyondCode\Mailbox\Routing\MailboxGroup
  */
 class MailboxGroup extends Facade
